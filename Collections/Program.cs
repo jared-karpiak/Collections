60d1f89f67046fe9d078e9d63b36c90fb87e0eb0
jared-karpiak <jkarpiak@nait.ca>
2025-03-24 08:36:16 -0600

    Add more functionality
    Added TodoItem class.
    
    Added DisplayMainMenu() function and PromptForChoice() function.

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> todoList = new List<string>();
+            bool programRunning = true;
+
+            while (programRunning)
+            {
+                DisplayMainMenu();
+                string choice = PromptForChoice();
+                switch (choice)
+                {
+                    case "a":
+
+                        break;
+                    case "b":
+                        break;
+                    case "c":
+                        break;
+                    case "q":
+                        programRunning = false;
+                        break;
+
+                }
+
+                
+            }
         }
+        static void DisplayMainMenu()
+        {
+            string menu = """
+                Select from the following options:
+                [a] Print To-Do List
+                [b] Add new item to To-Do List
+                [c] Remove item from To-Do List
+                [q] Quit program
+                """;
+            Console.WriteLine(menu);
+        }
+        static string PromptForChoice()
+        {
+            Console.Write("Enter a choice: ");
+            return Console.ReadLine().ToLower();
+        }
+        static void 
     }
 }
---
+++ Collections/TodoItem.cs
@@ -0,0 +1,24 @@
+﻿namespace Collections
+{
+    public class TodoItem
+    {
+        public string Description { get; private set; }
+        public bool Completed { get; set; }
+        public string Details { get; private set; }
+        public TodoItem(string description)
+        {
+            Description = description;
+            Completed = false;
+            Details = "";
+        }
+        public void AddDetails()
+        {
+            Console.WriteLine("Enter details about the task\n" +
+                $"{Description}:");
+            string details = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(details))
+                Details = details;
+        }
+    }
+}
